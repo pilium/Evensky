a33958941fe018743cb44d159ba5cc0325e426e0
Igor Myasnikov <igorpilium@gmail.com>
2018-12-25 20:06:36 +0300

    add sandwich component

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,17 @@
 svg4everybody();
 
 // @include('detect.js')
+
+// ------------------------------------------------//
+// SANDWICh
+// ------------------------------------------------//
+
+let sandwich = document.querySelector('..sandwich');
+
+sandwich.addEventListener('click', () => {
+	this.classList.toggle('sandwich--active');
+});
+
+// ------------------------------------------------//
+// SANDWICh END
+// ------------------------------------------------//
