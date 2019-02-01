839f5fcd83e007b7aca6a55239653fc11b5a696e
pilium <igorpilium@gmail.com>
2019-02-01 08:50:04 +0300

    Add jquery . slick slider to dependencies

--- src/js/vendor.js
+++ src/js/vendor.js
@@ -1,5 +1,3 @@
 // @include('../../node_modules/babel-polyfill/dist/polyfill.js')
 // @include('../../node_modules/bowser/bowser.js')
-// @include('../../node_modules/jquery/dist/jquery.js')
 // @include('../../node_modules/svg4everybody/dist/svg4everybody.js')
-// @include('../../node_modules/slick-carousel/slick/slick.js')
