b61fba7e66a516be508d32374ff5d6f48831887a
pilium <igorpilium@gmail.com>
2019-02-07 08:11:31 +0300

    Add block you-seen

--- src/js/main.js
+++ src/js/main.js
@@ -30,9 +30,36 @@ $(document).ready(() => {
 	// ------------------------------------------------//
 	// Popular-cat-slider END
 	// ------------------------------------------------//
+	// ------------------------------------------------//
+	// You-seen product slider
+	// ------------------------------------------------//
+	// eslint-disable-next-line func-names
+	let productSeenSlider = function () {
+		const sliderCount = $('.js-section-header__text');
+		const slider = $('.js-slider--you-seen');
+
+		// eslint-disable-next-line func-names
+		slider.on('init afterChange', (event, slick, currentSlide) => {
+			let i = (currentSlide ? currentSlide : 0) + 1;
+
+			sliderCount.text(`Товар ${i} из ${slick.slideCount}`);
+		});
+
+		slider.slick({
+			infinite: false,
+			slidesToShow: 4,
+			slidesToScroll: 1,
+			prevArrow: '.slider__nav--prev',
+			nextArrow: '.slider__nav--next',
+		});
+	};
+	// ------------------------------------------------//
+	// You-seen product slider END
+	// ------------------------------------------------//
 
 	sandwich();
 	popularCategoriesSlider();
+	productSeenSlider();
 });
 
 let popularCategoriesSlider = () => {
